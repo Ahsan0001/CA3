22149def1ebce8dc427d5fb0a6593c9df73a4a26
Ahsan Iqbal <ahsan2018iqbal@gmail.com>
2023-09-19 12:50:30 +0500

    feat: Added subtraction route
    e.g. {{base_url}}/subtract/5/2 => 3 answer

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/subtract/<first>/<second>', methods=['GET'])
+def subtract_route(first, second):  # put application's code here
+    return int(first) - int(second)
 
 if __name__ == '__main__':
     app.run()
